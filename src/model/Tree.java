0bdb1c4243fb52cce8951218a882b29336445c0b
LordWe4pOns <simon.cossais@orange.fr>
2025-07-05 12:40:09 +0200

    feat: add TreeState enum and implement state management

--- src/model/Tree.java
+++ src/model/Tree.java
@@ -1,5 +1,41 @@
 package model;
 
+enum TreeState {
+    ALIVE,
+    BURNING,
+    ASHES,
+}
+
 public class Tree {
+    private TreeState state;
     
+
+    public Tree () {
+        this.state = TreeState.ALIVE;
+    }
+
+    
+    public boolean isAlive() {
+        return this.state.equals(TreeState.ALIVE);
+    }
+
+    public boolean isBurning() {
+        return this.state.equals(TreeState.BURNING);
+    }
+
+    public boolean isAshes() {
+        return this.state.equals(TreeState.ASHES);
+    }
+
+    public void extinguish() {
+        if (this.isBurning()) {
+            this.state = TreeState.ASHES;
+        }
+    }
+
+    public void ignite() {
+        if (!this.isAshes()) {
+            this.state = TreeState.BURNING;
+        }
+    }
 }
